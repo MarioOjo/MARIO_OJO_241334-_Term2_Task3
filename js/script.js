5077e08d7d7fe2aa9af96a83a0d0a7f217721b79
MarioOjo <159897985+MarioOjo@users.noreply.github.com>
2024-06-05 23:16:44 +0200

    Revert "changes"
    This reverts commit 7f5b8dd941680d848870b51437bed9c8986dd0ca.

--- js/script.js
+++ js/script.js
@@ -2,25 +2,3 @@ document.getElementById('contact').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
     alert('Thank you for your message!'); // Display the thank you message
 });
-
-
-
-
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
